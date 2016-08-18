c13bbb4caad32af189642967853161c0af70bf75
mskies <3792535@qq.com>
2016-08-18 16:58:21 +0800

    获取菜单及菜单的配置

--- application/component/controller/ComponentController.php
+++ application/component/controller/ComponentController.php
@@ -2,42 +2,55 @@
 namespace app\component\controller;
 use think\controller\Rest;
 use app\component\ComponentInterface;
-use think\Request;
+use app\Model\MenuModel;
 
 class ComponentController extends Rest implements ComponentInterface
 {
     protected $config = [];
     public function __construct($config = [])
     {   
-        $routeInfo = Request::instance()->routeInfo();
-        var_dump($routeInfo['rule'][0]);
+        // 获取用户当前菜单, 并将当前菜单的配置写入config
+        $MenuModel = MenuModel::getCurrentMenu();
+        $this->config = $MenuModel->param;
+        if (is_array($config))
+        {
+            $this->config = array_merge($this->config, $config);
+        }
+
         // 在这进行权限的判断
         parent::__construct();
     }
+
     public function indexAction()
     {
 
     }
+
     public function createAction()
     {
 
     }
+
     public function saveAction()
     {
 
     }
+
     public function readAction()
     {
 
     }
+
     public function editAction()
     {
 
     }
+
     public function updateAction()
     {
 
     }
+
     public function deleteAction()
     {
 
