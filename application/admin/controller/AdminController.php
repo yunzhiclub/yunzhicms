ea7c58f56935f5bede32a0b7d344d6362bd1dd18
mskies <3792535@qq.com>
2016-08-18 20:02:12 +0800

    后台实始化

---
+++ application/admin/controller/AdminController.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+
+class AdminController extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
