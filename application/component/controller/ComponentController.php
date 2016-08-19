69deabcac5ed96383e91103e14416eb1214b476f
mskies <3792535@qq.com>
2016-08-19 22:11:39 +0800

    动态写入配置

--- application/component/controller/ComponentController.php
+++ application/component/controller/ComponentController.php
@@ -1,24 +1,51 @@
 <?php
 namespace app\component\controller;
-use think\controller\Rest;
+use think\Controller;
 use app\component\ComponentInterface;
 use app\Model\MenuModel;
+use think\Request;
 
-class ComponentController extends Rest implements ComponentInterface
+class ComponentController extends Controller implements ComponentInterface
 {
     protected $config = [];
-    public function __construct($config = [])
+    protected $currentMenuModel;                    // 当前菜单
+    public function __construct($config = [], Request $request = null)
     {   
         // 获取用户当前菜单, 并将当前菜单的配置写入config
-        $MenuModel = MenuModel::getCurrentMenu();
-        $this->config = $MenuModel->param;
+        $this->currentMenuModel = MenuModel::getCurrentMenu();
+        $this->param = $this->currentMenuModel->param;
+
+        // 取出name及value两个字段的信息
+        if (is_array($this->param))
+        {
+            foreach ($this->param as $key => &$param)
+            {
+                if (array_key_exists($key, $this->config))
+                {
+                    array_merge($this->config[$key], $param);
+                }
+            }
+        }
+
+        // 合并传入的配置信息
         if (is_array($config))
         {
-            $this->config = array_merge($this->config, $config);
+            foreach ($config as $key => $value)
+            {
+                if (array_key_exists($key, $this->config))
+                {
+                    if (is_array($value))
+                    {
+                        array_merge($this->config[$key], $value);
+                    } else {
+                        $this->config[$key]['value'] = $value;
+                    }
+                }
+            }
         }
 
         // 在这进行权限的判断
-        parent::__construct();
+        parent::__construct($request);
     }
 
     public function indexAction()
