a7b28bf6b13ae56d247f6051ea9547c9d26d1799
mskies <3792535@qq.com>
2016-08-20 09:35:52 +0800

    重构：独立配置信息

--- application/component/controller/ComponentController.php
+++ application/component/controller/ComponentController.php
@@ -11,46 +11,94 @@ class ComponentController extends Controller implements ComponentInterface
     protected $currentMenuModel;                    // 当前菜单
     public function __construct($config = [], Request $request = null)
     {   
+        // 初始化配置信息 
+        $this->initConfig($config);           
+
+        // 在这进行权限的判断
+        //todo 
+        
+        // 调用父类的构造函数
+        parent::__construct($request);
+    }
+
+    /**
+     * 初始化配置信息
+     * @param  array $config 用户自定义配置
+     * @return bool          
+     */
+    public function initConfig($config)
+    {
         // 获取用户当前菜单, 并将当前菜单的配置写入config
         $this->currentMenuModel = MenuModel::getCurrentMenu();
         $this->param = $this->currentMenuModel->param;
 
-        // 取出name及value两个字段的信息
-        if (is_array($this->param))
+        // 合并配置信息
+        $this->config = $this->_configMerge($this->config, $this->param);
+        $this->config = $this->_configMerge($this->config, $config);
+
+        return true;
+    }
+
+    /**
+     * 合并配置信息
+     * 将配置2中的配置信息，合并到配置1中
+     * 示例：
+     * config1:
+     * array (size=1)
+     * 'count' => 
+     *   array (size=3)
+     *     'description' => string '显示新闻的条数' (length=21)
+     *     'type' => string 'text' (length=4)
+     *     'value' => int 3
+     *****************************************************************
+     * 第一种形式：只改变value值
+     * config2:
+     * array (size=1)
+     * 'count' => int 2
+     *
+     * @return
+     * array (size=1)
+     * 'count' => 
+     *   array (size=3)
+     *     'description' => string '显示新闻的条数' (length=21)
+     *     'type' => string 'text' (length=4)
+     *     'value' => int 2
+     *****************************************************************
+     * 第二种形式：改变其它值
+     * array (size=1)
+     * 'count' => 
+     *   array (size=2)
+     *     'description' => string 'hello' (length=5)
+     *     'value' => string '4' (length=1)
+     *
+     * @return
+     * array (size=1)
+     * 'count' => 
+     *   array (size=3)
+     *     'description' => string 'hello' (length=5)
+     *     'type' => string 'text' (length=4)
+     *     'value' => string '4' (length=1)
+     ******************************************************************    
+     * @author panjie
+     */
+    protected function _configMerge($config1, $config2)
+    {
+        if (is_array($config2))
         {
-            foreach ($this->param as $key => &$param)
+            foreach ($config2 as $key => &$config)
             {
-                if (array_key_exists($key, $this->config))
+                if (array_key_exists($key, $config1))
                 {
-                    if (is_array($param))
+                    if (is_array($config))
                     {
-                        $this->config[$key] = array_merge($this->config[$key], $param);
+                        $config1[$key] = array_merge($config1[$key], $config);
                     } else {
-                        $this->config[$key]['value'] = $param;
+                        $config1[$key]['value'] = $config;
                     }
                 }
             }
         }
-
-        // 合并传入的配置信息
-        if (is_array($config))
-        {
-            foreach ($config as $key => $value)
-            {
-                if (array_key_exists($key, $this->config))
-                {
-                    if (is_array($value))
-                    {
-                        $this->config[$key] = array_merge($this->config[$key], $value);
-                    } else {
-                        $this->config[$key]['value'] = $value;
-                    }
-                }
-            }
-        }
-
-        // 在这进行权限的判断
-        parent::__construct($request);
+        return $config1;
     }
 
     public function indexAction()
