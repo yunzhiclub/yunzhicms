4ee650c126ab214fcf2324cc510db262c0d4d6d0
mskies <3792535@qq.com>
2016-08-24 14:22:17 +0800

    后台编辑中显示配置信息
    TODO：新建 filter过滤器表，然后在各个过滤器字段中，存1.过滤器ID，2过滤器名称（自动添加？）3.过滤器参数

--- application/admin/controller/AdminController.php
+++ application/admin/controller/AdminController.php
@@ -4,14 +4,8 @@ use think\Controller;
 
 class AdminController extends Controller
 {
-    protected $config = [];         // 配置信息
-    protected $filter = [];         // 过滤器信息
     public function __construct()
     {
         parent::__construct();
-        if ($this->action == 'edit' || $this->action == 'read')
-        {
-            // 取出配置信息以及过滤器
-        }
     }
 }
