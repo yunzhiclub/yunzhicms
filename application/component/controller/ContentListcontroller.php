f74ff7a8ec190ccdd793123054a10ee15065103a
litian1218 <1181551049@qq.com>
2016-09-01 10:52:22 +0800

    修改contentlist控制器名字

--- application/component/controller/ContentListcontroller.php
+++ application/component/controller/ContentListcontroller.php
@@ -19,7 +19,7 @@ class ContentListController extends ComponentController
 
 
     public function readAction($id)
-    {   
+    {
         $map = ['id' => $id];
         $ContentModel = ContentModel::get($map);
         $this->assign('ContentModel', $ContentModel);
