34d516f31b5ca8d06e87549d5a79bd068c4b374f
mskies <3792535@qq.com>
2016-09-01 16:08:20 +0800

    将插件的配置及过滤器信息由字段 转到 文件

--- application/model/PluginTypeModel.php
+++ application/model/PluginTypeModel.php
@@ -4,4 +4,50 @@ namespace app\model;
 class PluginTypeModel extends ModelModel
 {
     protected $pk = 'name';
+    private $config = null;
+    private $filter = null;
+
+    /**
+     * 读取相关配置文件后，得出配置信息
+     * @return array 
+     */
+    public function getConfig()
+    {
+        if (null === $this->config) {
+            $configFilePath = APP_PATH . 
+                'plugin' . DS . 
+                'config' . DS .
+                $this->getData('name') . 'Config.php';
+            $configFilePath = realpath($configFilePath);
+            if (false === $configFilePath) {
+                $this->config = [];
+            } else {
+                $this->config = include $configFilePath;
+            }
+        }
+
+        return $this->config;
+    }
+
+    /**
+     * 读取相关 过滤器配置 文件后，得出过滤器信息
+     * @return array 
+     */
+    public function getFilter()
+    {
+        if (null === $this->filter) {
+            $filterFilePath = APP_PATH . 
+                'plugin' . DS . 
+                'filter' . DS .
+                $this->getData('name') . 'Filter.php';
+            $filterFilePath = realpath($filterFilePath);
+            if (false === $filterFilePath) {
+                $this->filter = [];
+            } else {
+                $this->filter = include $filterFilePath;
+            }
+        }
+
+        return $this->filter;
+    }
 }
