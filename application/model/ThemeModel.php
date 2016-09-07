04ae75c111aaac2d32b3e221276dcce6d11fcad9
huangshuaibin <huangshuaibin@163.com>
2016-09-07 21:54:07 +0800

    Theme
    1.C层增加read方法，进行启用操作
    2.M层增加enble方法，对数据进行操作
    3.增加V层文件

--- application/model/ThemeModel.php
+++ application/model/ThemeModel.php
@@ -10,4 +10,24 @@ class ThemeModel extends ModelModel
         $map = ['is_current' => '1'];
         return self::get($map);
     }
+    /**
+     * 启用当前模板
+     * @param  string $id 数据表中name字段
+     * @return bool
+     */
+    public function enable($id)
+    {
+    	$ThemeModel = new ThemeModel;
+    	$ThemeModels = $ThemeModel::All();
+    	foreach ($ThemeModels as $ThemeModel => $value) {
+    		$value->is_current = 0;
+    		$value->save();
+    	}
+
+    	$name = $id;
+    	$ThemeModel = ThemeModel::get($name);
+    	
+    	$ThemeModel->is_current = 1;
+    	$ThemeModel->save();
+    }
 }
