51313ce84479051b194467adf5b4ffe090d6e623
litian1218 <1181551049@qq.com>
2016-09-07 16:44:11 +0800

    组件管理

--- application/admin/controller/ComponentController.php
+++ application/admin/controller/ComponentController.php
@@ -1,10 +1,26 @@
 <?php
 namespace app\admin\controller;
-
+use app\Common;                         // 通用函数库
+use app\model\ComponentModel;
 class ComponentController extends AdminController
 {
     public function indexAction()
     {
+        $Components = ComponentModel::paginate();
+        $this->assign('Components', $Components);
+        return $this->fetch();
+    }
+
+    /**
+     * 读取配置信息
+     */
+    public function readAction($id)
+    {
+        $name = $id;
+        $ComponentModel = ComponentModel::get($name);
+        // var_dump($Component);
+        $this->assign('ComponentModel', $ComponentModel);
+
         return $this->fetch();
     }
 }
