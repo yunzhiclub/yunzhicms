6d81ef3c7a04667327c8ef208cd8488a81fd979f
mskies <3792535@qq.com>
2016-09-09 14:05:32 +0800

    多图片上传，DONE

--- application/field/config/ImagesConfig.php
+++ application/field/config/ImagesConfig.php
@@ -4,14 +4,9 @@ return [
         'title'         => 'js第三方类库',
         'description'   => '相对于public的相对路径, 以"/"开头。多链接 使用 "," 相隔',
         'type'          => 'text',
-        'value'         => '/lib/uploadify/jquery.uploadify.min.js'
+        'value'         => '/bower_components/ng-file-upload/ng-file-upload-all.min.js'
     ],
-    'css'   => [
-        'title'         => 'css第三方类库',
-        'description'   => '相对于public的相对路径, 以"/"开头。多链接 使用 "," 相隔',
-        'type'          => 'text',
-        'value'         => '/lib/uploadify/uploadify.css'
-    ],
+
     'uploadPath'   => [
         'title'         => '上传文件夹',
         'description'   => '相对于public的相对路径, 以"/"开头',
