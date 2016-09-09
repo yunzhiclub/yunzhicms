0612d6b987be1c0cb10b9290b4665a20cb9463c9
litian1218 <1181551049@qq.com>
2016-09-09 17:00:38 +0800

    修改bug：缺失的}

--- application/admin/controller/BlockController.php
+++ application/admin/controller/BlockController.php
@@ -41,7 +41,7 @@ class BlockController extends AdminController
         $BlockModel->setData('position_name', $param['position_name']);
         $BlockModel->setData('status', $param['status']);
         $BlockModel->setData('weight', $param['weight']);
-       
+
         if (array_key_exists('config', $param))
         {
             $BlockModel->setData('config', json_encode($param['config']));
@@ -52,7 +52,7 @@ class BlockController extends AdminController
             $filter = Common::makeFliterArrayFromPostArray($param['filter']);
             $BlockModel->setData('filter', json_encode($filter));
         }
-       
+
         $BlockModel->save();
 
         // 更新block-menu关联表
@@ -86,7 +86,7 @@ class BlockController extends AdminController
         if (false === $BlockModel->save()) {
             return $this->error('删除失败');
         }
-
+    }
 
     public function saveAction()
     {
