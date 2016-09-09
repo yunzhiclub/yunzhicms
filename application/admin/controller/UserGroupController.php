10ea83b4fc85022fd4e17c59eb238da482415dd4
tangzhenjie <2819786276@qq.com>
2016-09-09 21:32:07 +0800

    用户组的编辑实现

--- application/admin/controller/UserGroupController.php
+++ application/admin/controller/UserGroupController.php
@@ -7,8 +7,28 @@ class UserGroupController extends AdminController
 {
     public function indexAction()
     {
-    	$UserGroup = new UserGroupModel;
-    	$this->assign('UserGroupModels', $UserGroup);
+    	$UserGroupModels = UserGroupModel::all();
+    	$this->assign('UserGroupModels', $UserGroupModels);
         return $this->fetch();
+    }
+
+    public function editAction($id)
+    {
+    	$UserGroupModel = UserGroupModel::get($id);
+    	$this->assign('UserGroupModel', $UserGroupModel);
+    	return $this->fetch();
+    }
+
+    public function updateAction($id)
+    {
+    	$data = input('param.');
+
+    	$UserGroupModel = UserGroupModel::get($id);
+    	$UserGroupModel->setData('title', $data['title']);
+    	$UserGroupModel->setData('name', $data['name']);
+    	$UserGroupModel->setData('description', $data['description']);
+
+    	$UserGroupModel->save();
+    	return $this->success('操作成功', url('@admin/usergroup/'));
     }
 }
