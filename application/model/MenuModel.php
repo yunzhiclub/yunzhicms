c36c002297f46ece76c8fa024c52e5ae115bcf97
tangzhenjie <2819786276@qq.com>
2016-09-13 19:55:13 +0800

    菜单增加分页

--- application/model/MenuModel.php
+++ application/model/MenuModel.php
@@ -130,7 +130,8 @@ class MenuModel extends ModelModel
     public function getListsByMenuTypeNamePid($menuTypeName, $pid, $delete)
     {
         $map = ['menu_type_name' => $menuTypeName, 'pid' => $pid, 'is_delete' => $delete];
-        $MenuModels = $this->where($map)->order('weight desc')->select();
+        //设置分页
+        $MenuModels = $this->where($map)->order('weight desc')->paginate(config('paginate.var_page'));
         return $MenuModels;
     }
 
