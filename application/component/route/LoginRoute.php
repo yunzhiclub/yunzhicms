694380fe2b8cf878ff9836358cb7a3c5447953b6
mskies <3792535@qq.com>
2016-09-13 14:01:52 +0800

    增加 用户登录与注销组件 。 todo:美化组件

--- application/component/route/LoginRoute.php
+++ application/component/route/LoginRoute.php
@@ -1,16 +1,22 @@
 <?php
 return [
+    // 注意注册路由的顺序：由长到短！
+    'login' => [
+        'title'         => '登录',
+        'description'   => '登录',
+        'value'         => ['/login', 'POST']   
+    ],
+
+    'logout'    => [
+        'title'         => '注销',
+        'description'   => '用户点击用进行注销操作',
+        'value'         => ['/logout', 'GET']    
+    ],
+
     'index' => [
         'title'         => '浏览',
         'description'   => '用户登陆前和登陆后的form表单显示',
         'value'         => ['', 'GET']        
     ],
-
-    'login' => [
-        'title'         => '登陆',
-        'description'   => '用于用户登陆',
-        'value'         => ['/hello/hi/login', 'GET']
-    ],
-
-
+    
 ];
