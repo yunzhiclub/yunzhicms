94b0e9a21f453cd5297ab81c71548518a7f36b6e
liuxiqian <1454179583@qq.com>
2016-09-30 11:55:23 +0800

    更新 数据表

--- sql/yunzhi_user.sql
+++ sql/yunzhi_user.sql
@@ -2,15 +2,15 @@
 Navicat MySQL Data Transfer
 
 Source Server         : 本地
-Source Server Version : 50626
+Source Server Version : 50505
 Source Host           : localhost:3306
 Source Database       : yunzhicms
 
 Target Server Type    : MYSQL
-Target Server Version : 50626
+Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2016-09-21 13:39:16
+Date: 2016-09-30 11:16:55
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -27,15 +27,15 @@ CREATE TABLE `yunzhi_user` (
   `name` varchar(40) NOT NULL DEFAULT '' COMMENT '真实姓名',
   `status` tinyint(2) unsigned NOT NULL DEFAULT '0' COMMENT '0正常 1冻结',
   `user_group_name` varchar(40) NOT NULL DEFAULT '' COMMENT 'fk user_group',
-  `create_time` smallint(6) unsigned NOT NULL,
-  `update_time` smallint(6) unsigned NOT NULL,
-  `is_deleted` tinyint(2) unsigned NOT NULL DEFAULT '0' COMMENT '1已删除',
+  `create_time` int(11) unsigned NOT NULL,
+  `update_time` int(11) unsigned NOT NULL,
+  `is_delete` tinyint(2) unsigned NOT NULL DEFAULT '0' COMMENT '1已删除',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`username`) USING BTREE
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='用户表';
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='用户表';
 
 -- ----------------------------
 -- Records of yunzhi_user
 -- ----------------------------
 INSERT INTO `yunzhi_user` VALUES ('1', 'admin@mengyunzhi.com', '', '', '梦云智', '0', 'admin', '0', '0', '0');
-INSERT INTO `yunzhi_user` VALUES ('2', '123456@qq.com', '', 'd8406e8445cc99a16ab984cc28f6931615c766fc', '123', '0', 'editor', '65535', '65535', '0');
+INSERT INTO `yunzhi_user` VALUES ('2', '123', '', '', 'qwer', '0', 'public', '0', '0', '0');
